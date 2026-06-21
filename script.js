029519cbd0075e394eb1e00b87494b397f2ad8c6
Alonso <ggerardo@pregrado.ubo.cl>
2026-06-20 20:03:11 -0400

    Ajuste de aparicion de corazon

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function agregarCorazon(){
     const corazon = document.createElement("div");
 
-    corazon.innerHTML="❤️";
+    corazon.innerHTML = "❤️";
     corazon.classList.add("corazon");
 
-    corazon.style.left = Math.random() * window.innerWidth + "px";
-    corazon.style.top = Math.random() * window.innerHeight + "px";
+    const margen = 100;
+
+    corazon.style.left =
+        Math.random() * (window.innerWidth - margen) + "px";
+
+    corazon.style.top =
+        Math.random() * (window.innerHeight - margen) + "px";
 
     document.body.appendChild(corazon);
 
